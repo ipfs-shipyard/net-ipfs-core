cccfb6df283a484cd3a00ffb9b427a661252fddf
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-03-12 21:19:09 +0800

    update project base folder to fit namespace

---
+++ Ipfs.Core/CoreApi/IDhtApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ipfs.CoreApi
+{
+    /// <summary>
+    ///   Manages the Distributed Hash Table.
+    /// </summary>
+    /// <remarks>
+    ///   The DHT is a place to store, not the value, but pointers to peers who have 
+    ///   the actual value.
+    ///   <para>
+    ///   See the ongoing DHT specification at <see href="https://github.com/libp2p/specs/pull/108"/>
+    ///   </para>
+    /// </remarks>
+    /// <seealso href="https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/DHT.md">DHT API spec</seealso>
+    public interface IDhtApi : IPeerRouting, IContentRouting, IValueStore
+    {
+    }
+}
---
+++ Ipfs.Core/IFileSystemLink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ipfs
+{
+    /// <summary>
+    ///    A link to another file system node in IPFS.
+    /// </summary>
+    public interface IFileSystemLink : IMerkleLink
+    {
+    }
+}
