fc8f4863c5653430aafd3311eb989a0cc1882d9e
Arlo <arlo.godfrey@outlook.com>
2025-08-16 18:46:22 -0500

    Pin API: list options overload; AddAsync progress via IProgress; records-based JSON; tests updated (in dependent http client)

--- src/CoreApi/IPinApi.cs
+++ src/CoreApi/IPinApi.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System;
 
 namespace Ipfs.CoreApi
 {
@@ -10,28 +13,34 @@ namespace Ipfs.CoreApi
     /// <seealso href="https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/PIN.md">Pin API spec</seealso>
     public interface IPinApi
     {
-        
+        /// <summary>
+        ///   Adds an IPFS object to the pinset and also stores it to the IPFS repo. pinset is the set of hashes currently pinned (not gc'able).
+        /// </summary>
+        /// <param name="path">
+        ///   A CID or path to an existing object, such as "QmXarR6rgkQ2fDSHjSY5nM2kuCXKYGViky5nohtwgF65Ec/about"
+        ///   or "QmZTR5bcpQD7cFgTorqxZDYaew1Wqgfbd2ud9QqGPAkK2V"
+        /// </param>
+        /// <param name="options">
+        ///   Options for pinning (name and recursion). If null, defaults are used.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation. The task's value
+        ///   is a sequence of <see cref="Cid"/> that were pinned.
+        /// </returns>
+        Task<IEnumerable<Cid>> AddAsync(string path, PinAddOptions options, CancellationToken cancel = default);
 
-    /// <summary>
-    ///   Adds an IPFS object to the pinset and also stores it to the IPFS repo. pinset is the set of hashes currently pinned (not gc'able).
-    /// </summary>
-    /// <param name="path">
-    ///   A CID or path to an existing object, such as "QmXarR6rgkQ2fDSHjSY5nM2kuCXKYGViky5nohtwgF65Ec/about"
-    ///   or "QmZTR5bcpQD7cFgTorqxZDYaew1Wqgfbd2ud9QqGPAkK2V"
-    /// </param>
-    /// <param name="options">
-    ///   Options for pinning (name and recursion). If null, defaults are used.
-    /// </param>
-    /// <param name="cancel">
-    ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
-    /// </param>
-    /// <returns>
-    ///   A task that represents the asynchronous operation. The task's value
-    ///   is a sequence of <see cref="Cid"/> that were pinned.
-    /// </returns>
-    Task<IEnumerable<Cid>> AddAsync(string path, PinAddOptions options, CancellationToken cancel = default);
-
-    // Removed multiple AddAsync overloads in favor of options-based overload above.
+        /// <summary>
+        ///   Adds an IPFS object to the pinset with progress reporting.
+        /// </summary>
+        /// <param name="path">The CID or path of the object to pin.</param>
+        /// <param name="options">Options for pinning (name and recursion).</param>
+        /// <param name="progress">Receives cumulative blocks-pinned updates.</param>
+        /// <param name="cancel">Cancellation token.</param>
+        /// <returns>Pinned CIDs on completion.</returns>
+        Task<IEnumerable<Cid>> AddAsync(string path, PinAddOptions options, IProgress<BlocksPinnedProgress> progress, CancellationToken cancel = default);
 
         /// <summary>
         ///   List all the objects pinned to local storage.
@@ -39,11 +48,8 @@ namespace Ipfs.CoreApi
         /// <param name="cancel">
         ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
         /// </param>
-        /// <returns>
-        ///   A task that represents the asynchronous operation. The task's value
-        ///   is a sequence of <see cref="Cid"/>.
-        /// </returns>
-        Task<IEnumerable<Cid>> ListAsync(CancellationToken cancel = default);
+        /// <returns>An async sequence of <see cref="PinListItem"/>.</returns>
+        IAsyncEnumerable<PinListItem> ListAsync(CancellationToken cancel = default);
 
         /// <summary>
         ///   List all the objects pinned to local storage.
@@ -54,11 +60,16 @@ namespace Ipfs.CoreApi
         /// <param name="cancel">
         ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
         /// </param>
-        /// <returns>
-        ///   A task that represents the asynchronous operation. The task's value
-        ///   is a sequence of <see cref="Cid"/>.
-        /// </returns>
-        Task<IEnumerable<Cid>> ListAsync(PinType type, CancellationToken cancel = default);
+        /// <returns>An async sequence of <see cref="PinListItem"/>.</returns>
+        IAsyncEnumerable<PinListItem> ListAsync(PinType type, CancellationToken cancel = default);
+
+        /// <summary>
+        ///   List pinned objects with advanced options.
+        /// </summary>
+        /// <param name="options">List options (type filter, names, stream, etc.).</param>
+        /// <param name="cancel">Cancellation token.</param>
+        /// <returns>An async sequence of <see cref="PinListItem"/>.</returns>
+        IAsyncEnumerable<PinListItem> ListAsync(PinListOptions options, CancellationToken cancel = default);
 
         /// <summary>
         ///   Unpin an object.
@@ -78,5 +89,7 @@ namespace Ipfs.CoreApi
         ///   is a sequence of <see cref="Cid"/> that were unpinned.
         /// </returns>
         Task<IEnumerable<Cid>> RemoveAsync(Cid id, bool recursive = true, CancellationToken cancel = default);
+
+
     }
 }
--- src/CoreApi/PinAddOptions.cs
+++ src/CoreApi/PinAddOptions.cs
@@ -1,19 +1,55 @@
-namespace Ipfs.CoreApi
+using System.Collections.Generic;
+
+namespace Ipfs.CoreApi;
+
+
+/// <summary>
+///   A unified representation of a pin list entry that works for both
+///   streaming and non-streaming list responses.
+/// </summary>
+public record PinListItem
 {
     /// <summary>
-    ///   Options for pin add.
+    ///   The CID of the pinned object.
     /// </summary>
-    public class PinAddOptions
-    {
-        /// <summary>
-        ///   Optional name for created pin(s).
-        /// </summary>
-        public string? Name { get; set; }
+    public Ipfs.Cid Cid { get; init; } = null!;
 
-        /// <summary>
-        ///   True to recursively pin links of the object; otherwise, false to only pin the specified object.
-        ///   Default is true.
-        /// </summary>
-        public bool Recursive { get; set; } = true;
-    }
+    /// <summary>
+    ///   The pin type (direct, indirect, recursive).
+    /// </summary>
+    public PinType Type { get; init; }
+
+    /// <summary>
+    ///   Optional pin name (present when names are requested and set).
+    /// </summary>
+    public string? Name { get; init; }
+}
+
+
+/// <summary>
+///   Options for pin add.
+/// </summary>
+public record PinAddOptions
+{
+    /// <summary>
+    ///   Optional name for created pin(s).
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    ///   True to recursively pin links of the object; otherwise, false to only pin the specified object.
+    ///   Default is true.
+    /// </summary>
+    public bool Recursive { get; set; } = true;
+}
+
+/// <summary>
+///   Progress notification for pin/add reporting cumulative blocks pinned.
+/// </summary>
+public record BlocksPinnedProgress
+{
+    /// <summary>
+    ///   The cumulative number of blocks pinned so far.
+    /// </summary>
+    public int BlocksPinned { get; init; }
 }
